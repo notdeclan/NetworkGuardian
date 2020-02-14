25871475b06e52c80407f4dfd510ce555b44b198
Declan Woodham <1701185@abertay.ac.uk>
2020-02-14 16:12:20 +0000

    Added reload, and open directory functionality to Plugin page in GUI
    Added jQuery and enabled bootstrap tool tips in GUI

--- src/networkguardian/gui/blueprints/api.py
+++ src/networkguardian/gui/blueprints/api.py
@@ -1,7 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request, redirect
+
+from framework.registry import import_external_plugins, load_plugins
+from networkguardian import plugins_directory
 
 """
     Flask Blueprint class used for handling all API requests
 """
 
 mod = Blueprint('api', __name__)
+
+"""
+    Plugins
+"""
+
+
+@mod.route("/api/plugins/refresh")
+def refresh_plugins():
+    import_external_plugins(plugins_directory)
+    load_plugins()
+    return redirect(request.headers.get("Referer"))
+
+
+@mod.route("/api/plugins/directory")
+def plugin_directory():
+    pass
