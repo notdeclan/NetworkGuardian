efa937bc452d6c62ed7e7badd8de457226d9b9f9
Velislav <velislav.velchev95@hotmail.com>
2020-02-18 14:47:47 +0000

    Firewall Plugin Completed. No Linux Support in current version of the plugin.

--- src/networkguardian/standard_plugins.py
+++ src/networkguardian/standard_plugins.py
@@ -201,7 +201,7 @@ class LocalFirewallStatus(BasePlugin):
 
 
     def __init__(self):
-        super().__init__("Local Firewall Status Plugin", Category.INFO, "Velislav V", 0.4,
+        super().__init__("Local Firewall Status Plugin", Category.INFO, "Velislav V", 0.5,
                          [Platform.WINDOWS, Platform.LINUX, Platform.MAC_OS])
 
     @property
